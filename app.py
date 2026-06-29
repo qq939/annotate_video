b7902a58035a560097a75286b7cf3b368ff296c4
qq939 <939342547@qq.com>
2026-06-30 00:19:54 +0800

    Change particle color to white

--- app.py
+++ app.py
@@ -3073,8 +3073,8 @@ class UnifiedPanel(QMainWindow):
 
                 # 绘制粒子效果 - 在接触面上绘制渐变圆点
                 if enable_particle and not self.render_segment_check.isChecked():
-                    # 红色粒子
-                    particle_color = (0, 0, 255)  # BGR 红色
+                    # 白色粒子
+                    particle_color = (255, 255, 255)  # BGR 白色
                     
                     # 收集所有多边形和bbox
                     all_contours = []
